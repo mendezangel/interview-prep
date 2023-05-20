297f6005d1f7dc161a358ca8ec868392e6ab3c42
Angel Mendez <id.angelmendez@gmail.com>
2023-05-20 09:26:26 -0700

    added second solution for anagrams using dictionaries

--- structy-problems/string-array/anagrams.py
+++ structy-problems/string-array/anagrams.py
@@ -3,9 +3,24 @@
 # strings are anagrams. Anagrams are strings that contains the
 # same characters, but in any order.
 
-def anagrams(s1, s2):
-  return sorted(s1) == sorted(s2)
+# def anagrams(s1, s2):
+#   return sorted(s1) == sorted(s2)
 
+def anagrams(s1, s2):
+    characterCount = {}
+    for letter in s1:
+        if letter not in characterCount:
+            characterCount.update({letter: 0})
+        characterCount[letter] += 1
+
+    for letter in s2:
+        if letter not in characterCount:
+            return False
+        characterCount[letter] -= 1
+
+    for count in characterCount.values():
+        if count != 0: return False
+    return True
 print(anagrams('restful', 'fluster')) # -> True
 print(anagrams('cats', 'tocs')) # -> False
 print(anagrams('monkeyswrite', 'newyorktimes')) # -> True
