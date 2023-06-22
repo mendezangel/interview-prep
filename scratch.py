74c73e32ef77e270cbb2eb4dd844ec2ff587d309
Angel Mendez <id.angelmendez@gmail.com>
2023-06-22 10:28:55 -0700

    solved array of products problem

--- scratch.py
+++ scratch.py
@@ -1,5 +1,11 @@
-array = [1, 2, 3]
-copy = list(array)
-copy.pop()
-print(f'original: {array}')
-print(f'copy: {copy}')
+def right_products(array):
+    for i in reversed(range(len(array) - 1)):
+        current_num = array[i]
+        right_num = array[i + 1]
+        print(f'i: {i}')
+        print(f'current num: {current_num}')
+        print(f'right num: {right_num}')
+        array[i] = current_num * right_num
+    return array
+    
+print(right_products([5, 1, 4, 2])) # [40, 8, 8, 2]
