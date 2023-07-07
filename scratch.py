67157cecb5a00051903157e768e37d961583701f
Angel Mendez <id.angelmendez@gmail.com>
2023-07-07 07:26:32 -0700

    just some scratch work

--- scratch.py
+++ scratch.py
@@ -1,2 +1,15 @@
-string = "Angel"
-print(type([*'abcdefghijklmnopqrstuvwxyz']))
+def all_permutations(string, i=0, letters=set()):
+    if i >= len(string):
+        return string
+    letters = set([*string])
+    print(string)
+    for letter in letters:
+        if letter != string[i]:
+            string_list = [*string]
+            string_list[i] = letter
+            new_string = ''.join(string_list)
+            all_permutations(new_string, i + 1, letters)
+        
+
+
+print(all_permutations('abc'))
